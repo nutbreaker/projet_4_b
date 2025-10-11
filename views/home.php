4c4cf3a55c888fa70171e807a0a6e1b010951d28
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-10-11 16:33:20 +0200

    feat(home): create HTML skeleton
    Create the HTML skeleton for the home page which will allow to add the CSS styles.
    
    - home.php add HTML
    - create img folder which contains all images used in the home page

---
+++ views/home.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            <a href="/">
+                <img src="/img/logo.svg" alt="logo">
+            </a>
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Accueil</a>
+                </li>
+                <li>
+                    <a href="/books">Nos livres à l'échange</a>
+                </li>
+                <li>
+                    <a href="/chat">Messagerie</a>
+                </li>
+                <li>
+                    <a href="/account">Mon compte</a>
+                </li>
+                <li>
+                    <a href="/signin">Connexion</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+        <section>
+            <div>
+                <h2>
+                    Rejoignez nos lecteurs passionnés
+                </h2>
+                <p>
+                    Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
+                </p>
+                <a href="#">Découvrir</a>
+            </div>
+            <figure>
+                <img src="/img/hamza.jpg" alt="Hamza">
+                <figcaption>
+                    Hamza
+                </figcaption>
+            </figure>
+        </section>
+
+        <section>
+            <h2>Les derniers livres ajoutés</h2>
+            <div>
+
+                <figure>
+                    <img src="/img/esther.jpg" alt="">
+                    <figcaption>
+                        <h3>Esther</h3>
+                        <p>Alabaster</p>
+                        <div>Vendu par : CamilleClubLit</div>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="/img/the_kinkfolk_table.jpg" alt="">
+                    <figcaption>
+                        <h3>The Kinfolk Table</h3>
+                        <p>Nathan Williams</p>
+                        <div>Vendu par : Nathalire</div>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="/img/wabi_sabi.jpg" alt="">
+                    <figcaption>
+                        <h3>Wabi Sabi</h3>
+                        <p>Beth Kempton</p>
+                        <div>Vendu par : Alexlecture</div>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="/img/milk_and_honey.jpg" alt="">
+                    <figcaption>
+                        <h3>Milk & honey</h3>
+                        <p>Rupi Kaur</p>
+                        <div>Vendu par : Hugo1990_12</div>
+                    </figcaption>
+                </figure>
+
+            </div>
+            <a href="">Voir tous les livres</a>
+        </section>
+
+        <section>
+            <h2>Comment ça marche ?</h2>
+            <p>Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
+            <div>
+                <span>Inscrivez-vous gratuitement sur notre plateforme.</span>
+                <span>Ajoutez les livres que vous souhaitez échanger à votre profil.</span>
+                <span>Parcourez les livres disponibles chez d'autres membres.</span>
+                <span>Proposez un échange et discutez avec d'autres passionnés de lecture.</span>
+            </div>
+
+            <a href="">Voir tous les livres</a>
+        </section>
+        <div>
+            <img src="/img/stretch.jpg" alt="">
+        </div>
+        <section>
+            <h2>Nos valeurs</h2>
+            <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+
+            <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
+
+            <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+            <div>
+                <span>L'équipe Tom Troc</span>
+
+                <img src="/img/heart.svg" alt="">
+            </div>
+
+
+        </section>
+    </main>
+    <footer>
+        <nav>
+            <ul>
+                <li><a>Politique de confidentialité</a></li>
+                <li><a>Mentions légales</a></li>
+                <li><a>Tom Troc &copy;</a></li>
+                <li>
+                    <a>
+                        <img src="/img/tt.svg" alt="logo">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+
+</html>
