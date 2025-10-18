ffd75573c8ff42f229d0d785c2997fb4d3676cb4
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-10-18 11:27:22 +0200

    feat(home): add css styling
    Add css style for the home page following the design defined by figma
    
    - [X] add favicon to home template
    - [X] add image `chat-icon.svg`
    - [X] create file `style.css`
    - [X] import fonts Playfair and Inter from google fonts
    - [X] add css variables to define
      - maximum size of the template
      - different colors
      - default font sizes
    - [X] define default styles for tags
    - [X] define styles for generic component `btn`
    - [X] define styles for generic component `card`
    - [X] `header`
      - modify html template to add css classes
      - define css styles
    - [X] `hero`
      - modify html template to add css classes
      - define css styles
    - [X] `latest-books`
      - modify html template to add css classes
      - define css styles
    - [X] `how-it-works`
      - modify html template to add css classes
      - define css styles
    - [X] `strech`
      - modify html template to add css classes
      - define css styles
    - [X] `our-values`
      - modify html template to add css classes
      - define css styles
    - [X] `footer`
      - modify html template to add css classes
      - define css styles

--- views/home.php
+++ views/home.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/img/tt.svg" />
 </head>
 
 <body>
@@ -15,7 +18,7 @@
             </a>
         </h1>
         <nav>
-            <ul>
+            <ul class="header-menu">
                 <li>
                     <a href="/">Accueil</a>
                 </li>
@@ -23,7 +26,7 @@
                     <a href="/books">Nos livres à l'échange</a>
                 </li>
                 <li>
-                    <a href="/chat">Messagerie</a>
+                    <a class="header-chat" href="/chat">Messagerie <span class="header-chat-notification">1</span></a>
                 </li>
                 <li>
                     <a href="/account">Mon compte</a>
@@ -36,102 +39,101 @@
     </header>
     <main>
 
-        <section>
-            <div>
-                <h2>
+        <section class="hero">
+            <div class="hero-content">
+                <h2 class="hero-title">
                     Rejoignez nos lecteurs passionnés
                 </h2>
-                <p>
+                <p class="hero-description">
                     Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
                 </p>
-                <a href="#">Découvrir</a>
+                <a class="btn" href="#">Découvrir</a>
             </div>
-            <figure>
-                <img src="/img/hamza.jpg" alt="Hamza">
-                <figcaption>
+            <figure class="hero-figure">
+                <img class="hero-img" src="/img/hamza.jpg" alt="Hamza">
+                <figcaption class="hero-figcaption">
                     Hamza
                 </figcaption>
             </figure>
         </section>
 
-        <section>
+        <section class="latest-books">
             <h2>Les derniers livres ajoutés</h2>
-            <div>
-
-                <figure>
-                    <img src="/img/esther.jpg" alt="">
-                    <figcaption>
-                        <h3>Esther</h3>
-                        <p>Alabaster</p>
-                        <div>Vendu par : CamilleClubLit</div>
+            <div class="latest-books-cards">
+                <figure class="card">
+                    <img class="card-img" src="/img/esther.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Esther</h3>
+                        <p class="card-figcaption-subtitle">Alabaster</p>
+                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
                     </figcaption>
                 </figure>
 
-                <figure>
-                    <img src="/img/the_kinkfolk_table.jpg" alt="">
-                    <figcaption>
-                        <h3>The Kinfolk Table</h3>
-                        <p>Nathan Williams</p>
-                        <div>Vendu par : Nathalire</div>
+                <figure class="card">
+                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                        <p class="card-figcaption-subtitle">Nathan Williams</p>
+                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
                     </figcaption>
                 </figure>
 
-                <figure>
-                    <img src="/img/wabi_sabi.jpg" alt="">
-                    <figcaption>
-                        <h3>Wabi Sabi</h3>
-                        <p>Beth Kempton</p>
-                        <div>Vendu par : Alexlecture</div>
+                <figure class="card">
+                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                        <p class="card-figcaption-subtitle">Beth Kempton</p>
+                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
                     </figcaption>
                 </figure>
 
-                <figure>
-                    <img src="/img/milk_and_honey.jpg" alt="">
-                    <figcaption>
-                        <h3>Milk & honey</h3>
-                        <p>Rupi Kaur</p>
-                        <div>Vendu par : Hugo1990_12</div>
+                <figure class="card">
+                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Milk & honey</h3>
+                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
                     </figcaption>
                 </figure>
-
             </div>
-            <a href="">Voir tous les livres</a>
+
+            <a class="btn" href="/books">Voir tous les livres</a>
         </section>
 
-        <section>
+        <section class="how-it-works">
             <h2>Comment ça marche ?</h2>
-            <p>Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
-            <div>
-                <span>Inscrivez-vous gratuitement sur notre plateforme.</span>
-                <span>Ajoutez les livres que vous souhaitez échanger à votre profil.</span>
-                <span>Parcourez les livres disponibles chez d'autres membres.</span>
-                <span>Proposez un échange et discutez avec d'autres passionnés de lecture.</span>
+
+            <p class="how-it-works-description">Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
+
+            <div class="how-it-works-list">
+                <span class="how-it-works-item">Inscrivez-vous gratuitement sur <br> notre plateforme.</span>
+                <span class="how-it-works-item">Ajoutez les livres que vous souhaitez échanger à <br> votre profil.</span>
+                <span class="how-it-works-item">Parcourez les livres disponibles chez d'autres membres.</span>
+                <span class="how-it-works-item">Proposez un échange et discutez avec d'autres passionnés de lecture.</span>
             </div>
 
-            <a href="">Voir tous les livres</a>
+            <a class="btn btn-invert" href="">Voir tous les livres</a>
         </section>
-        <div>
-            <img src="/img/stretch.jpg" alt="">
+        <div class="strech">
+            <img class="strech-img" src="/img/stretch.jpg" alt="">
         </div>
-        <section>
-            <h2>Nos valeurs</h2>
-            <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+        <section class="our-values">
+            <h2 class="our-values-title">Nos valeurs</h2>
 
-            <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
+            <p class="our-values-description">Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+            <p class="our-values-description">Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
+            <p class="our-values-description">Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
 
-            <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
-            <div>
-                <span>L'équipe Tom Troc</span>
+            <div class="our-values-footer">
+                <span class="our-values-footer-content">L'équipe Tom Troc</span>
 
-                <img src="/img/heart.svg" alt="">
+                <img class="our-values-footer-img" src="/img/heart.svg" alt="">
             </div>
-
-
         </section>
     </main>
-    <footer>
+    <footer class="footer">
         <nav>
-            <ul>
+            <ul class="footer-menu">
                 <li><a>Politique de confidentialité</a></li>
                 <li><a>Mentions légales</a></li>
                 <li><a>Tom Troc &copy;</a></li>
