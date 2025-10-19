38dd9d9371cfd1e586a236551e4ddbfcb8b6533c
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-10-19 13:44:09 +0200

    feat(books): add HTML skeleton and css styling
    Add HTML skeleton and CSS styles for the `books` page following the design defined in Figma.
    
    - [X] add a pages section to add css overrides this will allow to know the extent
    of the adjustments to be made in order to rationalize the different css rules and
    better share the different styles
    - [X] add template for the `books` page containing the `header`, `main` and `footer`
    from `home.php`
    - [X] add a `book-exchange` section in `main`
      - create title block and search field
      - create the block containing the cards
    - [X] define `book-exchange` styles
    - [X] add the svg image representing a magnifying glass used in the search field

---
+++ views/books.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books</title>
+
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/img/tt.svg" />
+</head>
+
+<body class="books">
+    <header>
+        <h1>
+            <a href="/">
+                <img src="/img/logo.svg" alt="logo">
+            </a>
+        </h1>
+        <nav>
+            <ul class="header-menu">
+                <li>
+                    <a href="/">Accueil</a>
+                </li>
+                <li>
+                    <a href="/books">Nos livres à l'échange</a>
+                </li>
+                <li>
+                    <a class="header-chat" href="/chat">Messagerie <span class="header-chat-notification">1</span></a>
+                </li>
+                <li>
+                    <a href="/account">Mon compte</a>
+                </li>
+                <li>
+                    <a href="/signin">Connexion</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+        <section class="book-exchange">
+            <div class="book-exchange-header">
+                <h2 class="book-exchange-title">Nos livres à l'échange</h2>
+                <span class="book-exchange-search">
+                    <input class="book-exchange-input" type="search" name="book-exchange-input" id="book-exchange-input" placeholder="Rechercher un livre">
+                </span>
+            </div>
+
+            <div class="book-exchange-cards">
+                <figure class="card">
+                    <img class="card-img" src="/img/esther.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Esther</h3>
+                        <p class="card-figcaption-subtitle">Alabaster</p>
+                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                        <p class="card-figcaption-subtitle">Nathan Williams</p>
+                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                        <p class="card-figcaption-subtitle">Beth Kempton</p>
+                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Milk & honey</h3>
+                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
+                    </figcaption>
+                </figure>
+
+                 <figure class="card">
+                    <img class="card-img" src="/img/esther.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Esther</h3>
+                        <p class="card-figcaption-subtitle">Alabaster</p>
+                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                        <p class="card-figcaption-subtitle">Nathan Williams</p>
+                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                        <p class="card-figcaption-subtitle">Beth Kempton</p>
+                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
+                    </figcaption>
+                </figure>
+
+                <figure class="card">
+                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                    <figcaption class="card-figcaption">
+                        <h3 class="card-figcaption-title">Milk & honey</h3>
+                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
+                    </figcaption>
+                </figure>
+            </div>
+
+        </section>
+    </main>
+    <footer class="footer">
+        <nav>
+            <ul class="footer-menu">
+                <li><a>Politique de confidentialité</a></li>
+                <li><a>Mentions légales</a></li>
+                <li><a>Tom Troc &copy;</a></li>
+                <li>
+                    <a>
+                        <img src="/img/tt.svg" alt="logo">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+
+</html>
