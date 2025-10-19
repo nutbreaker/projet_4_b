f4113748e741c6ade8a61d84897a15e7a41718a7
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-10-19 17:25:20 +0200

    feat(book): add HTML skeleton and css styling
    Add HTML skeleton and CSS styles for the `book` page following the design defined in Figma.
    
    - [X] add template for the `book` page containing the `header`, `main` and `footer`
    from `home.php`
    - [X] add a `book-breadcrum` div in below the header
    - [X] add a `book-section` section in `main`
      - create `figure` containing an `img` and a `figcaption`
    - [X] add the html elements describing a book in `figcaption`
    - [X] define CSS styles for `book-breadcrum` and `book-section`
    - [X] add the user image

---
+++ views/book.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book</title>
+
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/img/tt.svg" />
+</head>
+
+<body class="book">
+    <header>
+        <h1>
+            <a href="/">
+                <img src="/img/logo.svg" alt="logo">
+            </a>
+        </h1>
+        <nav>
+            <ul class="header-menu">
+                <li>
+                    <a href="/">Accueil</a>
+                </li>
+                <li>
+                    <a href="/books">Nos livres à l'échange</a>
+                </li>
+                <li>
+                    <a class="header-chat" href="/chat">Messagerie <span class="header-chat-notification">1</span></a>
+                </li>
+                <li>
+                    <a href="/account">Mon compte</a>
+                </li>
+                <li>
+                    <a href="/signin">Connexion</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="book-breadcrum">
+        <span>Nos livres</span>&gt;<span>The Kinkfolk Table</span>
+    </div>
+    <main>
+        <section class="book-section">
+
+            <figure class="book-section-figure">
+                <img src="/img/the_kinkfolk_table.jpg" alt="the kinkfolk table">
+
+                <figcaption>
+                    <h2>The Kinkfolk Table</h2>
+                    <span>par Nathan Williams</span>
+
+                    <hr>
+
+                    <h3>Description</h3>
+                    <p>J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager des moments authentiques autour de la table.</p>
+
+                    <p>Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans un voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la convivialité.</p>
+
+                    <p>Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres chers.</p>
+
+                    <p>'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain que ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres inspirantes.</p>
+
+                    <h3>Propriétaire</h3>
+                    <div>
+                        <img src="/img/alexlecture.jpg" alt="Alexlecture" ><span>Alexlecture</span>
+                    </div>
+
+                    <a class="btn" href="#">Envoyer un message</a>
+                </figcaption>
+            </figure>
+
+        </section>
+    </main>
+    <footer class="footer">
+        <nav>
+            <ul class="footer-menu">
+                <li><a>Politique de confidentialité</a></li>
+                <li><a>Mentions légales</a></li>
+                <li><a>Tom Troc &copy;</a></li>
+                <li>
+                    <a>
+                        <img src="/img/tt.svg" alt="logo">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+
+</html>
