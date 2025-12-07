5e659598385ea3f68e0e4a5959459a5fde07c78f
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-12-07 17:11:54 +0100

    feat(home): improve responsiveness based on figma guidlines
    Improve the homepage layout and enhance the overall responsiveness of the website
    based on Figma guidelines.
    
    - [X] `header`, add a button that will control the popover menu on responsive mode
    - [X] `main`, adapts all `sections` so that the display matches the mockup
    - [X] `footer`, adapts `menu` so that the display matches the mockup
    - [X] `cards`, make the component agnostic

--- views/home.php
+++ views/home.php
@@ -12,126 +12,141 @@
 
 <body>
     <header>
-        <h1>
-            <a href="/">
-                <img src="/img/logo.svg" alt="logo">
-            </a>
-        </h1>
-        <nav>
-            <ul class="header-menu">
-                <li>
-                    <a href="/">Accueil</a>
-                </li>
-                <li>
-                    <a href="/books">Nos livres à l'échange</a>
-                </li>
-                <li>
-                    <a class="header-chat" href="/chat">Messagerie <span class="header-chat-notification">1</span></a>
-                </li>
-                <li>
-                    <a class="header-account" href="/account">Mon compte</a>
-                </li>
-                <li>
-                    <a href="/signin">Connexion</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <main>
+        <div class="header-container">
+            <h1>
+                <a href="/">
+                    <img src="/img/logo.svg" alt="logo">
+                </a>
+            </h1>
 
+            <button class="header-nav-button" commandfor="header-nav" command="toggle-popover">
+                <img src="img/icon_menu.svg" alt="Menu button">
+            </button>
+
+            <nav popover id="header-nav" class="header-nav">
+                <ul class="header-menu">
+                    <li>
+                        <a href="/">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="/books">Nos livres à l'échange</a>
+                    </li>
+                    <li>
+                        <a class="header-chat" href="/chat">Messagerie <span class="header-chat-notification">1</span></a>
+                    </li>
+                    <li>
+                        <a class="header-account" href="/account">Mon compte</a>
+                    </li>
+                    <li>
+                        <a href="/signin">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
         <section class="hero">
-            <div class="hero-content">
-                <h2 class="hero-title">
-                    Rejoignez nos lecteurs passionnés
-                </h2>
-                <p class="hero-description">
-                    Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
-                </p>
-                <a class="btn" href="#">Découvrir</a>
+            <div class="hero-container">
+                <div class="hero-content">
+                    <h2 class="hero-title">
+                        Rejoignez nos lecteurs passionnés
+                    </h2>
+                    <p class="hero-description">
+                        Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
+                    </p>
+                    <a class="btn" href="#">Découvrir</a>
+                </div>
+                <figure class="hero-figure">
+                    <img class="hero-img" src="/img/hamza.jpg" alt="Hamza">
+                    <figcaption class="hero-figcaption">
+                        Hamza
+                    </figcaption>
+                </figure>
             </div>
-            <figure class="hero-figure">
-                <img class="hero-img" src="/img/hamza.jpg" alt="Hamza">
-                <figcaption class="hero-figcaption">
-                    Hamza
-                </figcaption>
-            </figure>
         </section>
 
         <section class="latest-books">
-            <h2>Les derniers livres ajoutés</h2>
-            <div class="latest-books-cards">
-                <figure class="card">
-                    <img class="card-img" src="/img/esther.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Esther</h3>
-                        <p class="card-figcaption-subtitle">Alabaster</p>
-                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
-                    </figcaption>
-                </figure>
+            <div class="latest-books-container">
+                <h2>Les derniers livres ajoutés</h2>
+                <div class="latest-books-cards card-container">
+                    <figure class="card">
+                        <img class="card-img" src="/img/esther.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Esther</h3>
+                            <p class="card-figcaption-subtitle">Alabaster</p>
+                            <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
+                        </figcaption>
+                    </figure>
 
-                <figure class="card">
-                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
-                        <p class="card-figcaption-subtitle">Nathan Williams</p>
-                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
-                    </figcaption>
-                </figure>
+                    <figure class="card">
+                        <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                            <p class="card-figcaption-subtitle">Nathan Williams</p>
+                            <span class="card-figcaption-info">Vendu par : Nathalire</span>
+                        </figcaption>
+                    </figure>
 
-                <figure class="card">
-                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
-                        <p class="card-figcaption-subtitle">Beth Kempton</p>
-                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
-                    </figcaption>
-                </figure>
+                    <figure class="card">
+                        <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                            <p class="card-figcaption-subtitle">Beth Kempton</p>
+                            <span class="card-figcaption-info">Vendu par : Alexlecture</span>
+                        </figcaption>
+                    </figure>
 
-                <figure class="card">
-                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Milk & honey</h3>
-                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
-                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
-                    </figcaption>
-                </figure>
+                    <figure class="card">
+                        <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Milk & honey</h3>
+                            <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                            <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
+                        </figcaption>
+                    </figure>
+                </div>
+
+                <a class="btn" href="/books">Voir tous les livres</a>
             </div>
-
-            <a class="btn" href="/books">Voir tous les livres</a>
         </section>
 
         <section class="how-it-works">
-            <h2>Comment ça marche ?</h2>
+            <div class="how-it-works-container">
+                <h2>Comment ça marche ?</h2>
 
-            <p class="how-it-works-description">Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
+                <p class="how-it-works-description">Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
 
-            <div class="how-it-works-list">
-                <span class="how-it-works-item">Inscrivez-vous gratuitement sur <br> notre plateforme.</span>
-                <span class="how-it-works-item">Ajoutez les livres que vous souhaitez échanger à <br> votre profil.</span>
-                <span class="how-it-works-item">Parcourez les livres disponibles chez d'autres membres.</span>
-                <span class="how-it-works-item">Proposez un échange et discutez avec d'autres passionnés de lecture.</span>
+                <div class="how-it-works-list">
+                    <span class="how-it-works-item">Inscrivez-vous gratuitement sur <br> notre plateforme.</span>
+                    <span class="how-it-works-item">Ajoutez les livres que vous souhaitez échanger à <br> votre profil.</span>
+                    <span class="how-it-works-item">Parcourez les livres disponibles chez d'autres membres.</span>
+                    <span class="how-it-works-item">Proposez un échange et discutez avec d'autres passionnés de lecture.</span>
+                </div>
+
+                <a class="btn btn-invert" href="">Voir tous les livres</a>
             </div>
-
-            <a class="btn btn-invert" href="">Voir tous les livres</a>
         </section>
         <div class="strech">
             <img class="strech-img" src="/img/stretch.jpg" alt="">
         </div>
         <section class="our-values">
-            <h2 class="our-values-title">Nos valeurs</h2>
+            <div class="our-values-container">
+                <h2 class="our-values-title">Nos valeurs</h2>
 
-            <p class="our-values-description">Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
-            <p class="our-values-description">Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
-            <p class="our-values-description">Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+                <p class="our-values-description">Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+                <p class="our-values-description">Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
+                <p class="our-values-description">Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
 
-            <div class="our-values-footer">
-                <span class="our-values-footer-content">L'équipe Tom Troc</span>
+                <div class="our-values-footer">
+                    <span class="our-values-footer-content">L'équipe Tom Troc</span>
 
-                <img class="our-values-footer-img" src="/img/heart.svg" alt="">
+                    <img class="our-values-footer-img" src="/img/heart.svg" alt="">
+                </div>
             </div>
         </section>
     </main>
-    <footer class="footer">
+    <footer>
         <nav>
             <ul class="footer-menu">
                 <li><a>Politique de confidentialité</a></li>
