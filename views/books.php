7529e60f76957f9b2bf48bbd91d68af5d4e86f35
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-12-07 17:56:20 +0100

    feat(books): improve responsiveness based on figma guidlines
    Improve the books page layout and enhance the overall responsiveness of the website
    based on Figma guidelines.
    
    - [X] `main`, adapt `book-exchange` section so that the display matches the mockup

--- views/books.php
+++ views/books.php
@@ -47,87 +47,88 @@
     <main>
 
         <section class="book-exchange">
-            <div class="book-exchange-header">
-                <h2 class="book-exchange-title">Nos livres à l'échange</h2>
-                <span class="book-exchange-search">
-                    <input class="book-exchange-input" type="search" name="book-exchange-input" id="book-exchange-input" placeholder="Rechercher un livre">
-                </span>
+            <div class="book-exchange-container">
+                <div class="book-exchange-header">
+                    <h2 class="book-exchange-title">Nos livres à l'échange</h2>
+                    <span class="book-exchange-search">
+                        <input class="book-exchange-input" type="search" name="book-exchange-input" id="book-exchange-input" placeholder="Rechercher un livre">
+                    </span>
+                </div>
+
+                <div class="book-exchange-cards card-container">
+                    <figure class="card">
+                        <img class="card-img" src="/img/esther.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Esther</h3>
+                            <p class="card-figcaption-subtitle">Alabaster</p>
+                            <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                            <p class="card-figcaption-subtitle">Nathan Williams</p>
+                            <span class="card-figcaption-info">Vendu par : Nathalire</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                            <p class="card-figcaption-subtitle">Beth Kempton</p>
+                            <span class="card-figcaption-info">Vendu par : Alexlecture</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Milk & honey</h3>
+                            <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                            <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/esther.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Esther</h3>
+                            <p class="card-figcaption-subtitle">Alabaster</p>
+                            <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">The Kinfolk Table</h3>
+                            <p class="card-figcaption-subtitle">Nathan Williams</p>
+                            <span class="card-figcaption-info">Vendu par : Nathalire</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Wabi Sabi</h3>
+                            <p class="card-figcaption-subtitle">Beth Kempton</p>
+                            <span class="card-figcaption-info">Vendu par : Alexlecture</span>
+                        </figcaption>
+                    </figure>
+
+                    <figure class="card">
+                        <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
+                        <figcaption class="card-figcaption">
+                            <h3 class="card-figcaption-title">Milk & honey</h3>
+                            <p class="card-figcaption-subtitle">Rupi Kaur</p>
+                            <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-
-            <div class="book-exchange-cards">
-                <figure class="card">
-                    <img class="card-img" src="/img/esther.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Esther</h3>
-                        <p class="card-figcaption-subtitle">Alabaster</p>
-                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
-                        <p class="card-figcaption-subtitle">Nathan Williams</p>
-                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
-                        <p class="card-figcaption-subtitle">Beth Kempton</p>
-                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Milk & honey</h3>
-                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
-                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/esther.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Esther</h3>
-                        <p class="card-figcaption-subtitle">Alabaster</p>
-                        <span class="card-figcaption-info">Vendu par : CamilleClubLit</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/the_kinkfolk_table.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">The Kinfolk Table</h3>
-                        <p class="card-figcaption-subtitle">Nathan Williams</p>
-                        <span class="card-figcaption-info">Vendu par : Nathalire</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/wabi_sabi.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Wabi Sabi</h3>
-                        <p class="card-figcaption-subtitle">Beth Kempton</p>
-                        <span class="card-figcaption-info">Vendu par : Alexlecture</span>
-                    </figcaption>
-                </figure>
-
-                <figure class="card">
-                    <img class="card-img" src="/img/milk_and_honey.jpg" alt="">
-                    <figcaption class="card-figcaption">
-                        <h3 class="card-figcaption-title">Milk & honey</h3>
-                        <p class="card-figcaption-subtitle">Rupi Kaur</p>
-                        <span class="card-figcaption-info">Vendu par : Hugo1990_12</span>
-                    </figcaption>
-                </figure>
-            </div>
-
         </section>
     </main>
     <footer>
