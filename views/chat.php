94f0d1df33f87992dedc4d712141a0f3be8b2bec
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-12-07 19:43:12 +0100

    feat(chat): improve responsiveness based on figma guidlines
    Improve the chat page layout and enhance the overall responsiveness of the website
    based on Figma guidelines.
    
    - [X] `main`, adapt `chat-container` layout so that the display matches the mockup

--- views/chat.php
+++ views/chat.php
@@ -46,73 +46,76 @@
     </header>
 
     <main>
-        <aside class="chat-user-list">
-            <h2>Messagerie</h2>
-            <ul>
-                <li class="chat-active">
-                    <a class="chat-user" href="#">
-                        <img src="/img/alexlecture.jpg" alt="Alexlecture">
-                        <div class="chat-details">
-                            <div class="chat-details-header">
-                                <span class="chat-user-name">Alexlecture</span>
-                                <span class="chat-time">15:43</span>
+        <div class="chat-container">
+            <aside class="chat-user-list">
+                <h2>Messagerie</h2>
+                <ul>
+                    <li class="chat-active">
+                        <a class="chat-user" href="#">
+                            <img src="/img/alexlecture.jpg" alt="Alexlecture">
+                            <div class="chat-details">
+                                <div class="chat-details-header">
+                                    <span class="chat-user-name">Alexlecture</span>
+                                    <span class="chat-time">15:43</span>
+                                </div>
+                                <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
                             </div>
-                            <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a class="chat-user" href="#">
-                        <img src="/img/nathalire.jpg" alt="Nathalire">
-                        <div class="chat-details">
-                            <div class="chat-details-header">
-                                <span class="chat-user-name">Nathalire</span>
-                                <span class="chat-time">20:08</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="chat-user" href="#">
+                            <img src="/img/nathalire.jpg" alt="Nathalire">
+                            <div class="chat-details">
+                                <div class="chat-details-header">
+                                    <span class="chat-user-name">Nathalire</span>
+                                    <span class="chat-time">20:08</span>
+                                </div>
+                                <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
                             </div>
-                            <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a class="chat-user" href="#">
-                        <img src="/img/sas634.jpg" alt="Sas634">
-                        <div class="chat-details">
-                            <div class="chat-details-header">
-                                <span class="chat-user-name">Sas634</span>
-                                <span class="chat-time">15:08</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="chat-user" href="#">
+                            <img src="/img/sas634.jpg" alt="Sas634">
+                            <div class="chat-details">
+                                <div class="chat-details-header">
+                                    <span class="chat-user-name">Sas634</span>
+                                    <span class="chat-time">15:08</span>
+                                </div>
+                                <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
                             </div>
-                            <p class="chat-excerpt">Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor</p>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-        </aside>
-        <section class="chat-window">
-            <div class="chat-header">
-                <img src="/img/alexlecture.jpg" alt="Alexlecture">
+                        </a>
+                    </li>
+                </ul>
+            </aside>
+            <section class="chat-window">
+                <a class="chat-back-link" href="/chat">retour</a>
+                <div class="chat-header">
+                    <img src="/img/alexlecture.jpg" alt="Alexlecture">
 
-                <span class="chat-user-name">Alexlecture</span>
+                    <span class="chat-user-name">Alexlecture</span>
 
-            </div>
-            <div class="chat-messages">
-                <div class="message-sent">
-                    <span class="message-time">21.08 15:44</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
                 </div>
-                <div class="message-received">
-                    <div class="message-received-header">
-                        <img src="/img/alexlecture.jpg" alt="Alexlecture">
-                        <span class="message-time">21.08 15:48</span>
+                <div class="chat-messages">
+                    <div class="message-sent">
+                        <span class="message-time">21.08 15:44</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                    <div class="message-received">
+                        <div class="message-received-header">
+                            <img src="/img/alexlecture.jpg" alt="Alexlecture">
+                            <span class="message-time">21.08 15:48</span>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                    </div>
                 </div>
-            </div>
-            <div class="chat-input">
-                <input type="text" name="chat-message-text" id="chat-message-text">
+                <div class="chat-input">
+                    <input type="text" name="chat-message-text" id="chat-message-text">
 
-                <button class="btn">Envoyer</button>
-            </div>
-        </section>
+                    <button class="btn">Envoyer</button>
+                </div>
+            </section>
+        </div>
     </main>
     <footer>
         <nav>
