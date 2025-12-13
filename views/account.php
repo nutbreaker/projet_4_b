e7570ca5a3dc9c34a9d4023d80bef48bb57aaf9e
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-12-13 20:02:03 +0100

    feat(account): improve responsiveness based on Figma guidelines
    Improve the account page layout and enhance the overall responsiveness of the
    website based on Figma guidelines.
    
    - [X] add `account-container` that will contain admin and books sections
    - [X] adapt layout so that the display matches the mockup
    - [X] remove CSS nesting to decrease the specificity
    - [X] rename table classes to make the component more agnostic
    - [X] `btn` width 100% in responsive mode
    - [X] `footer-menu` align text center

--- views/account.php
+++ views/account.php
@@ -45,131 +45,138 @@
         </div>
     </header>
     <main>
-        <section class="account-admin">
-            <h2 class="account-title">Mon compte</h2>
+        <div class="account-container">
+            <section class="account-admin">
+                <h2 class="account-title">Mon compte</h2>
 
-            <div class="account-info">
-                <div class="account-profile">
-                    <div class="account-image-container">
-                        <img class="account-img" src="/img/nathalire.jpg" alt="nathalire">
+                <div class="account-info">
+                    <div class="account-profile">
+                        <div class="account-image-container">
+                            <img class="account-img" src="/img/nathalire.jpg" alt="nathalire">
+                        </div>
+                        <form class="avatar-form" action="/account" method="POST" enctype="multipart/form-data">
+                            <label
+                                class="avatar-label"
+                                for="avatar"
+                                role="button"
+                                tabindex="0"
+                                onkeydown="if([' ', 'Enter'].includes(event.key)) avatar.click()">modifier</label>
+
+                            <input
+                                id="avatar"
+                                name="avatar"
+                                class="avatar-input"
+                                type="file"
+                                accept="image/png, image/jpeg"
+                                aria-hidden="true"
+                                tabindex="-1" />
+                        </form>
+
+                        <hr class="account-info-separator">
+
+                        <h3 class="account-info-title">nathalire</h3>
+                        <p class="account-info-date">Membre de puis 1 an</p>
+                        <h4 class="account-info-library">Bibliothèque</h4>
+                        <p class="account-info-books"> 4 livres</p>
+
                     </div>
-                    <form class="avatar-form" action="/account" method="POST" enctype="multipart/form-data">
-                        <label
-                            class="avatar-label"
-                            for="avatar"
-                            role="button"
-                            tabindex="0"
-                            onkeydown="if([' ', 'Enter'].includes(event.key)) avatar.click()">modifier</label>
 
-                        <input
-                            id="avatar"
-                            name="avatar"
-                            class="avatar-input"
-                            type="file"
-                            accept="image/png, image/jpeg"
-                            aria-hidden="true"
-                            tabindex="-1" />
-                    </form>
-
-                    <hr class="account-info-separator">
-
-                    <h3 class="account-info-title">nathalire</h3>
-                    <p class="account-info-date">Membre de puis 1 an</p>
-                    <h4 class="account-info-library">Bibliothèque</h4>
-                    <p class="account-info-books"> 4 livres</p>
-
+                    <div class="account-form-container">
+                        <form class="account-form" action="/account" method="POST">
+                            <h3>Vos informations personnelles</h3>
+                            <label for="email">
+                                Addresse email
+                                <input type="email" name="email" id="email" placeholder="nathalie@gmail.com">
+                            </label>
+                            <label for="password">
+                                Mot de passe
+                                <input type="password" name="password" id="password" placeholder="•••••••••">
+                            </label>
+                            <label for="username">
+                                Pseudo
+                                <input type="text" name="username" id="username" placeholder="nathalire">
+                            </label>
+                            <button class="btn btn-invert" href="#">Enregistrer</button>
+                        </form>
+                    </div>
                 </div>
+            </section>
+            <section class="account-books">
+                <table class="table">
+                    <thead>
+                        <tr class="table-header">
+                            <th>photo</th>
+                            <th>titre</th>
+                            <th>auteur</th>
+                            <th>description</th>
+                            <th>disponibilité</th>
+                            <th>action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="table-row">
+                            <td class="table-row-picture"><img class="table-row-img" src="/img/the_kinkfolk_table.jpg" alt="The Kinkfolk Table"></td>
+                            <td class="table-row-title">The Kinkfolk Table</td>
+                            <td class="table-row-author">Nathan Williams</td>
+                            <td class="table-row-description"><span class="table-row-description-container">J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre
+                                    captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager
+                                    des
+                                    moments authentiques autour de la table.
 
-                <div class="account-form-container">
-                    <form class="account-form" action="/account" method="POST">
-                        <h3>Vos informations personnelles</h3>
-                        <label for="email">
-                            Addresse email
-                            <input type="email" name="email" id="email" placeholder="nathalie@gmail.com">
-                        </label>
-                        <label for="password">
-                            Mot de passe
-                            <input type="password" name="password" id="password" placeholder="•••••••••">
-                        </label>
-                        <label for="username">
-                            Pseudo
-                            <input type="text" name="username" id="username" placeholder="nathalire">
-                        </label>
-                        <button class="btn btn-invert" href="#">Enregistrer</button>
-                    </form>
-                </div>
-            </div>
-        </section>
-        <section class="account-books">
-            <table class="books-table">
-                <thead>
-                    <tr class="books-table-header">
-                        <th>photo</th>
-                        <th>titre</th>
-                        <th>auteur</th>
-                        <th>description</th>
-                        <th>disponibilité</th>
-                        <th>action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="books-table-row">
-                        <td><img src="/img/the_kinkfolk_table.jpg" alt="The Kinkfolk Table"></td>
-                        <td>The Kinkfolk Table</td>
-                        <td>Nathan Williams</td>
-                        <td>J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre
-                            captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager
-                            des
-                            moments authentiques autour de la table.
+                                    Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans
+                                    un
+                                    voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la
+                                    convivialité.
 
-                            Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans
-                            un
-                            voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la
-                            convivialité.
+                                    Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres
+                                    chers.
 
-                            Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres
-                            chers.
+                                    'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain
+                                    que
+                                    ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres
+                                    inspirantes.</span></td>
+                            <td class="table-row-availibility"><span class="table-row-available">disponible</span></td>
+                            <td class="table-row-action">
+                                <span class="table-raw-action-container">
+                                    <a class="table-edit" href="edit">éditer</a> <a class="table-delete" href="#">supprimer</a>
+                                </span>
+                            </td>
+                        </tr>
 
-                            'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain
-                            que
-                            ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres
-                            inspirantes.</td>
-                        <td><span class="books-table-available">disponible</span></td>
-                        <td class="books-table-action">
-                            <a class="books-table-edit" href="edit">éditer</a> <a class="books-table-delete" href="#">supprimer</a>
-                        </td>
-                    </tr>
+                        <tr class="books-table-row table-row">
+                            <td class="table-row-picture"><img class="table-row-img" src="/img/the_kinkfolk_table.jpg" alt="The Kinkfolk Table"></td>
+                            <td class="table-row-title">The Kinkfolk Table</td>
+                            <td class="table-row-author">Nathan Williams</td>
+                            <td class="table-row-description"><span class="table-row-description-container">J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre
+                                    captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager
+                                    des
+                                    moments authentiques autour de la table.
 
-                    <tr class="books-table-row">
-                        <td><img src="/img/the_kinkfolk_table.jpg" alt="The Kinkfolk Table"></td>
-                        <td>The Kinkfolk Table</td>
-                        <td>Nathan Williams</td>
-                        <td>J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre
-                            captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager
-                            des
-                            moments authentiques autour de la table.
+                                    Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans
+                                    un
+                                    voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la
+                                    convivialité.
 
-                            Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans
-                            un
-                            voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la
-                            convivialité.
+                                    Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres
+                                    chers.
 
-                            Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres
-                            chers.
-
-                            'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain
-                            que
-                            ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres
-                            inspirantes.</td>
-                        <td>
-                <span class="books-table-unavailable">non dispo.</span></td>
-                        <td class="books-table-action">
-                            <a class="books-table-edit" href="edit">éditer</a> <a class="books-table-delete" href="#">supprimer</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
+                                    'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain
+                                    que
+                                    ce livre trouvera une place spéciale dans le cœur de tout amoureux de la cuisine et des rencontres
+                                    inspirantes.</span></td>
+                            <td class="table-row-availibility">
+                                <span class="table-row-unavailable">non dispo.</span>
+                            </td>
+                            <td class="table-row-action">
+                                <span class="table-raw-action-container">
+                                    <a class="table-edit" href="edit">éditer</a> <a class="table-delete" href="#">supprimer</a>
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </div>
     </main>
     <footer>
         <nav>
