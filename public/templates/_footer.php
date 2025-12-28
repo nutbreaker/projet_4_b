234619e8b1992a222335d9b36a6cd1f2b12c81a9
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2025-12-28 20:06:45 +0100

    feat: add templates
    Add the templates allowing display the template skeleton.
    
    - add _base_template.php
    - add _header.php
    - add _footer.php

---
+++ public/templates/_footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <nav>
+        <ul class="footer-menu">
+            <li><a>Politique de confidentialité</a></li>
+            <li><a>Mentions légales</a></li>
+            <li><a>Tom Troc &copy;</a></li>
+            <li>
+                <a>
+                    <img src="/img/tt.svg" alt="logo">
+                </a>
+            </li>
+        </ul>
+    </nav>
+</footer>
